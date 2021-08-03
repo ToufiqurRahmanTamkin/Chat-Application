161ddf10d973872cbd339fc009106e273b8397e4
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-08-04 04:18:18 +0600

    project setup

---
+++ middlewares/common/errorHandler.js
@@ -0,0 +1,7 @@
+const createError = require("http-errors");
+
+// 404 not found handler
+function notFoundHandler(req, res, next) {
+  next(createError(404, "Your requested content was not found!"));
+}
+
